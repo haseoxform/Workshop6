618bf080729322809c678b8d1281e970dce8211f
Chang Ding Yang <oscaryang12@yahoo.com>
2016-11-29 17:56:03 -0500

    facebookGotServed

--- server/src/server.js
+++ server/src/server.js
@@ -6,6 +6,7 @@ var readDocument = database.readDocument;
 // Creates an Express server.
 var app = express();
 
+
 // Starts the server on port 3000!
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
@@ -52,40 +53,40 @@ function getFeedData(user) {
 * if it fails.
 */
 function getUserIdFromToken(authorizationLine) {
-try {
-// Cut off "Bearer " from the header value.
-var token = authorizationLine.slice(7);
-// Convert the base64 string to a UTF-8 string.
-var regularString = new Buffer(token, 'base64').toString('utf8');
-// Convert the UTF-8 string into a JavaScript object.
-var tokenObj = JSON.parse(regularString);
-var id = tokenObj['id'];
-// Check that id is a number.
-if (typeof id === 'number') {
-return id;
-} else {
-// Not a number. Return -1, an invalid ID.
-return -1;
-}
-} catch (e) {
-// Return an invalid ID.
-return -1;
-}
+  try {
+  // Cut off "Bearer " from the header value.
+  var token = authorizationLine.slice(7);
+  // Convert the base64 string to a UTF-8 string.
+  var regularString = new Buffer(token, 'base64').toString('utf8');
+  // Convert the UTF-8 string into a JavaScript object.
+  var tokenObj = JSON.parse(regularString);
+  var id = tokenObj['id'];
+  // Check that id is a number.
+  if (typeof id === 'number') {
+    return id;
+  } else {
+  // Not a number. Return -1, an invalid ID.
+    return -1;
+  }
+  } catch (e) {
+  // Return an invalid ID.
+    return -1;
+  }
 }
 /**
 * Get the feed data for a particular user.
 */
 app.get('/user/:userid/feed', function(req, res) {
-var userid = req.params.userid;
-var fromUser = getUserIdFromToken(req.get('Authorization'));
-// userid is a string. We need it to be a number.
-// Parameters are always strings.
-var useridNumber = parseInt(userid, 10);
-if (fromUser === useridNumber) {
-// Send response.
-res.send(getFeedData(userid));
-} else {
-// 401: Unauthorized request.
-res.status(401).end();
-}
+  var userid = req.params.userid;
+  var fromUser = getUserIdFromToken(req.get('Authorization'));
+  // userid is a string. We need it to be a number.
+  // Parameters are always strings.
+  var useridNumber = parseInt(userid, 10);
+  if (fromUser === useridNumber) {
+    // Send response.
+    res.send(getFeedData(userid));
+  } else {
+    // 401: Unauthorized request.
+    res.status(401).end();
+  }
 });
